9e937eefdafe89c5d2f7dfbb3a9f6acb076e02b3
Lisa DeSpain <lisamdespain@gmail.com>
2022-11-10 14:48:22 -0600

    quick app change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img alt="picture of homeless encampment" src="https://images.ctfassets.net/81iqaqpfd8fy/43n1LWlPbi64oaeiQeUiQ0/d3d9c4a5582ff7d74899dcbb032af692/Homeless.jpg?h=620&w=1440" />
+      <img src="https://images.ctfassets.net/81iqaqpfd8fy/43n1LWlPbi64oaeiQeUiQ0/d3d9c4a5582ff7d74899dcbb032af692/Homeless.jpg?h=620&w=1440" alt="picture of homeless encampment" />
     </div>
   );
 }
